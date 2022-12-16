29fd3c859438fcf4461117b60cc465b550070f98
Maulana Rheza <103730296+MaulanaRhezaArdiansyah@users.noreply.github.com>
2022-12-16 17:45:58 +0800

    sixth commit

--- task4.js
+++ task4.js
@@ -1,16 +1,14 @@
 const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
   let resultValid = [];
-  if (dataArray == 0) {
+  if (dataArray.length < 5) {
     return "Jumlah angka dalam dataArray harus lebih dari 5!";
   }
   for (let i = 0; i < dataArray.length; i++) {
-    if (dataArray.length < 5) {
-      return "Jumlah angka dalam dataArray harus lebih dari 5!";
-    } else if (nilaiAwal >= nilaiAkhir) {
+    if (nilaiAwal >= nilaiAkhir) {
       return "Nilai akhir harus lebih besar dari nilai awal!";
     } else if (dataArray[i] > nilaiAwal && dataArray[i] < nilaiAkhir) {
       resultValid.push(dataArray[i]);
-      resultValid.sort();
+      resultValid.sort((a, b) => a - b);
     } else if (resultValid.length == 0) {
       resultValid = "Nilai tidak ditemukan!";
     }
@@ -19,6 +17,7 @@ const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
 };
 
 console.log(seleksiNilai(10, 30, [25, 21, 15, 16, 12, 26]));
+console.log(seleksiNilai(5, 30, [14, 17, 8, 9, 10, 21]));
 console.log(seleksiNilai(10, 10, [2, 6, 3, 21, 25, 20]));
 console.log(seleksiNilai(10, 20, [2, 6, 3, 21]));
 console.log(seleksiNilai(10, 20, [2, 6, 3, 21, 40, 35]));
